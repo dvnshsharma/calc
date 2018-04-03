40cc19febc2868a301543968c41fa24177dd143b
Divanshu <37525209+dvnshsharma@users.noreply.github.com>
2018-04-03 16:53:32 +1000

    Create Simple Controller.h

---
+++ Simple Controller.h
@@ -0,0 +1,26 @@
+import "Simple Controller.h"
+@implementation Simple Controller
+
+-(IBAction)Calculate:(id)sender
+{
+    if([plus state] == 1)
+    {
+        int answer = [numOne intValue] + [numTwo intValue];
+        [answer setIntValue:answer];
+    }
+    if([minus state] == 1)
+    {
+        int answer = [numOne intValue] - [numTwo intValue];
+        [answer setIntValue:answer];
+    }
+    if([multiply state] == 1)
+    {
+        int answer = [numOne intValue] * [numTwo intValue];
+        [answer setIntValue:answer];
+    }
+    if([divide state] == 1)
+    {
+        int answer = [numOne intValue] / [numTwo intValue];
+        [answer setIntValue:answer];
+    }
+}
